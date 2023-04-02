f20bbaa30da127215a5bf206fd99d71b746794fe
ivajlotokiew <ivajlo_nick@abv.bg>
2023-04-02 19:29:21 +0300

    Add library for modal components

--- src/components/Players.tsx
+++ src/components/Players.tsx
@@ -3,6 +3,21 @@ import Button from './CustomButtonComponent';
 import { useNavigate } from "react-router-dom";
 import { confirmAlert } from 'react-confirm-alert';
 import 'react-confirm-alert/src/react-confirm-alert.css';
+import Modal from 'react-modal';
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+    },
+};
+
+// Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
+Modal.setAppElement('#root');
 
 export type PlayerLinks = {
     self: { href: string },
@@ -20,8 +35,23 @@ export type Player = {
 }
 
 function Players() {
+    let subtitle: HTMLHeadingElement | null;
+    const [modalIsOpen, setIsOpen] = useState(false);
     const [players, setPlayers] = useState<Player[] | []>([]);
     const [isSubscribed, setIsSubscribed] = useState(true);
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        if (subtitle) subtitle.style.color = '#f00';
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
 
     useEffect(() => {
         // declare the async data fetching function
@@ -125,6 +155,27 @@ function Players() {
                     </div>
                 ))
             }
+            <div>
+                <button onClick={openModal}>Open Modal</button>
+                <Modal
+                    isOpen={modalIsOpen}
+                    onAfterOpen={afterOpenModal}
+                    onRequestClose={closeModal}
+                    style={customStyles}
+                    contentLabel="Example Modal"
+                >
+                    <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+                    <button onClick={closeModal}>close</button>
+                    <div>I am a modal</div>
+                    <form>
+                        <input />
+                        <button>tab navigation</button>
+                        <button>stays</button>
+                        <button>inside</button>
+                        <button>the modal</button>
+                    </form>
+                </Modal>
+            </div>
         </>
     )
 
